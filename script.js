20cf9e09ff8989063ba433392a7ae08d49d90b77
bettoarcaya <betoarcaya@gmail.com>
2018-03-27 16:07:15 -0400

    primer commit

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
-/*
-  Code by Gabriel Nunes
-*/
+function inIframe () { 
+  try { 
+    return window.self !== window.top; 
+  } catch (e) { 
+    return true; 
+  } 
+}
 
-function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
-
-var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
-var currentQuote = '', currentAuthor = '';
+let colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+let currentQuote = '', currentAuthor = '';
+let quotes = [];
+let cnt = 2;
 function openURL(url){
   window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
 }
+
+function previusQuote(){
+  if(quotes.length - cnt < 0){
+    currentQuote = quotes[0].quote;
+    currentAuthor = quotes[0].author;  
+  }else{
+    currentQuote = quotes[quotes.length - cnt].quote;
+    currentAuthor = quotes[quotes.length - cnt].author;
+    cnt++;
+  }
+  
+
+  if(inIframe()){
+    $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+  }
+
+  $(".text").animate({
+    opacity: 0
+  }, 500,
+  function() {
+    $(this).animate({
+      opacity: 1
+    }, 500);
+    $('#text').text(currentQuote);
+  });
+
+$(".author").animate({
+    opacity: 0
+  }, 500,
+  function() {
+    $(this).animate({
+      opacity: 1
+    }, 500);
+    $('#author').html(currentAuthor);
+  });
+
+  document.body.style.background = colors[Math.floor(Math.random() * colors.length)];
+
+}
+
 function getQuote() {
+  cnt = 2;
   $.ajax({
     headers: {
       "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
@@ -17,59 +62,49 @@ function getQuote() {
       "Content-Type": "application/x-www-form-urlencoded"
     },
     url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
-    success: function(r) {
-      if (typeof r === 'string') {
-       r = JSON.parse(r); 
+    success: (data) => {
+      currentQuote = data.quote;
+      currentAuthor = data.author;
+      quotes.push({quote:currentQuote, author:currentAuthor});
+      if(quotes.length != 1){
+        console.log(quotes[quotes.length - 2]);
       }
-      currentQuote = r.quote;
-      currentAuthor = r.author;
-      if(inIframe())
-      {
+      
+      if(inIframe()){
         $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-        $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
       }
-      $(".quote-text").animate({
+
+      $(".text").animate({
           opacity: 0
         }, 500,
         function() {
           $(this).animate({
             opacity: 1
           }, 500);
-          $('#text').text(r.quote);
+          $('#text').text(data.quote);
         });
 
-      $(".quote-author").animate({
+      $(".author").animate({
           opacity: 0
         }, 500,
         function() {
           $(this).animate({
             opacity: 1
           }, 500);
-          $('#author').html(r.author);
+          $('#author').html(data.author);
         });
 
-      var color = Math.floor(Math.random() * colors.length);
-      $("html body").animate({
-        backgroundColor: colors[color],
-        color: colors[color]
-      }, 1000);
-      $(".button").animate({
-        backgroundColor: colors[color]
-      }, 1000);
+      document.body.style.background = colors[Math.floor(Math.random() * colors.length)];
     }
   });
 }
 $(document).ready(function() {
   getQuote();
-  $('#new-quote').on('click', getQuote);
+  $('#new').on('click', getQuote);
+  $('#previus').on('click', previusQuote);
   $('#tweet-quote').on('click', function() {
     if(!inIframe()) {
       openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-    }
-  });
-  $('#tumblr-quote').on('click', function() {
-    if(!inIframe()) {
-      openURL('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
     }
   });
 });
